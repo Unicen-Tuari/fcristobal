fb10155bb142b3d60b18b6221931cbf34f5b7df1
Facundo Cristobal <Facundo Cristobal>
2016-04-27 13:47:17 -0300

    ultimos detalles

--- pagina/js/script.js
+++ pagina/js/script.js
@@ -1,6 +1,11 @@
 
 
 function mostrartabla(nombreclase, tablanum) {
+  if (nombreclase == "tablainst") {
+    for (var i = 0; i < document.getElementsByClassName("instrumento").length; i++) {
+      document.getElementsByClassName("instrumento")[i].style.display = "none";
+    }
+  }
   for (var i = 0; i < document.getElementsByClassName(nombreclase).length; i++) {
     document.getElementsByClassName(nombreclase)[i].style.display = "none";
   }
